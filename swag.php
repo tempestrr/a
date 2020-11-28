045ae68e49239cad35585a2e6464875c7db0f996
tempestrr <54087939+tempestrr@users.noreply.github.com>
2020-11-28 12:15:38 +0700

    Update swag.php

--- swag.php
+++ swag.php
@@ -15,7 +15,7 @@ while (true) {
 	$base = ['878', '813', '838', '851', '853'];
 	$rand_base = array_rand($base);
 	$number = $base[$rand_base].number(8);
-	$domain = ['wwjmp.com', 'esiix.com'];
+	$domain = ['wwjmp.com', 'esiix.com', '1secmail.com', '1secmail.net', '1secmail.org'];
 	$rand = array_rand($domain);
 	$email = str_replace(' ', '', strtolower($name)).number(2).'@'.$domain[$rand];
 	$username = explode('@', $email);
@@ -42,9 +42,29 @@ while (true) {
 	$gender = ['male', 'female'];
 	$randoms = array_rand($gender);
 
+	echo "Try to register\n";
+	$register = post('https://android.swirge.com/requests.php?f=register', 'username='.$username[0].'&email='.$email.'&password='.$password.'&confirm_password='.$password.'&gender='.$gender[$randoms].'&accept_terms=on', $headers);
+	if (stripos($register, 'Registration successful!')) {
+		echo "Success to register\n";
+		echo "Delay 5 seconds\n";
+		sleep(5);
+		echo "Try to get email verif account\n";
+		$getmsg = get('https://www.1secmail.com/api/v1/?action=getMessages&login=argonokurniawan18&domain=wwjmp.com');
+		$id = fetch_value($getmsg, '"id":',',"');
 
-	echo $register = post('https://android.swirge.com/requests.php?f=register', 'username='.$username[0].'&email='.$email.'&password='.$password.'&confirm_password='.$password.'&gender='.$gender[$randoms].'&accept_terms=on', $headers);
-	echo "\n";
+		if (stripos($getmsg, 'Account Activation')) {
+			echo "Email verification found!\n";
+			$readmsg = get('https://www.1secmail.com/api/v1/?action=readMessage&login=argonokurniawan18&domain=wwjmp.com&id='.$id);
+			$json = json_decode($readmsg, true);
+			$link = fetch_value($json['htmlBody'], '<a href="','" class="btn-primary"');
+			echo $final = get($link, $headers);
+			echo "\n";
+		} else {
+			echo "Email not found\n";
+		}
+	} else {
+		echo "Failed to register\n";
+	}
 }
 
 
